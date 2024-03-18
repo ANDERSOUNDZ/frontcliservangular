84eb077550dc29b3786c76d86638dd7a2d3d0ac5
Anderson Chanchay <andersonmikol@live.com>
2024-03-18 05:35:58 -0500

    Subo proyecto para revision

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+import { AppComponent } from './app.component';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ClientesEditComponent } from './clientes-edit/clientes-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientesEditComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    TooltipModule.forRoot()
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
